41cef74ac97cb52ed387192839bdd6e88b0fe3e3
Dmitry <bulbamanblr@gmail.com>
2023-02-09 04:17:33 +0100

    Add Domain layer with Entites Create solution

---
+++ ToDoList/Domain/Entities/ToDoItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Domain.Entities
+{
+    public class ToDoItem
+    {
+        public int Id { get; set; }
+
+        public string Note { get; set; }
+
+        public string Title { get; set; }
+
+        public DateTime CreationTime { get; set; }
+
+        public bool Done { get; set; }
+    }
+}
---
+++ ToDoList/Domain/Entities/ToDoList.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Domain.Entities
+{
+    public class ToDoList
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public IList<ToDoItem> Items { get; set; } = new List<ToDoItem>();
+    }
+}
